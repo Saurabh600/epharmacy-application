5474305bf735ff8764adce3f98f605840394c057
Saurabh Chaudhary <saurabh.ch169@gmail.com>
2024-12-05 18:30:56 +0530

    global exception handling added

---
+++ epharmacy_backend/src/main/java/com/example/epharmacy_backend/advice/GlobalExceptionHandler.java
@@ -0,0 +1,113 @@
+package com.example.epharmacy_backend.advice;
+
+import com.example.epharmacy_backend.dto.response.ApiErrorRes;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ApiErrorRes> resourceNotFoundException(NoResourceFoundException exception) {
+        var method = exception.getHttpMethod().toString();
+        var url = exception.getResourcePath();
+
+        var response = ApiErrorRes.builder()
+                .httpStatus(HttpStatus.NOT_FOUND.toString())
+                .message("invalid request!")
+                .error(method + " " + url + " does not exits!")
+                .time(LocalDateTime.now())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiErrorRes> jwtException(JwtException exception) {
+        String err;
+        if (exception instanceof ExpiredJwtException) {
+            err = "Jwt token is expired!";
+        } else if (exception instanceof MalformedJwtException) {
+            err = "Jwt token is malformed!";
+        } else {
+            err = "Jwt exception!";
+        }
+
+        var response = ApiErrorRes.builder()
+                .httpStatus(HttpStatus.UNAUTHORIZED.toString())
+                .message("invalid bearer token")
+                .error(err)
+                .time(LocalDateTime.now())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ApiErrorRes> entityNotFoundException(EntityNotFoundException exception) {
+        var response = ApiErrorRes.builder()
+                .httpStatus(HttpStatus.BAD_REQUEST.toString())
+                .message("resource not found!")
+                .error(exception.getLocalizedMessage())
+                .time(LocalDateTime.now())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityExistsException.class)
+    public ResponseEntity<ApiErrorRes> entityExitsException(EntityExistsException exception) {
+        var response = ApiErrorRes.builder()
+                .httpStatus(HttpStatus.BAD_REQUEST.toString())
+                .message(exception.getMessage())
+                .error("duplicate field exception!")
+                .time(LocalDateTime.now())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorRes> requestBodyValidationException(
+            MethodArgumentNotValidException exception) {
+        var errors = new HashMap<String, Object>();
+        exception.getBindingResult()
+                .getFieldErrors()
+                .forEach(fieldError -> errors
+                        .put(fieldError.getField(), fieldError.getDefaultMessage())
+                );
+
+        var response = ApiErrorRes.builder()
+                .httpStatus(HttpStatus.BAD_REQUEST.toString())
+                .message("request body validation failed!")
+                .error(errors)
+                .time(LocalDateTime.now())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorRes> unknownExceptionHandler(Exception exception) {
+        var response = ApiErrorRes.builder()
+                .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR.toString())
+                .message("internal server error!")
+                .error(null)
+                .time(LocalDateTime.now())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
