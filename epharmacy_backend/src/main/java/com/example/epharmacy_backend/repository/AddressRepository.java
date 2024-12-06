25664b7dfc9a8907f95fa9ae2c768b8a5f7b1b37
Saurabh Chaudhary <saurabh.ch169@gmail.com>
2024-12-06 20:54:30 +0530

    address, credential and medicine repo layer added

--- epharmacy_backend/src/main/java/com/example/epharmacy_backend/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.epharmacy_backend.repository;
-
-import com.example.epharmacy_backend.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
--- epharmacy_backend/src/main/java/com/example/epharmacy_backend/repository/MedicineRepository.java
+++ epharmacy_backend/src/main/java/com/example/epharmacy_backend/repository/MedicineRepository.java
@@ -1,9 +1,9 @@
 package com.example.epharmacy_backend.repository;
 
-import com.example.epharmacy_backend.entity.Credential;
+import com.example.epharmacy_backend.entity.Medicine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CredentialRepository extends JpaRepository<Credential, Long> {
+public interface MedicineRepository extends JpaRepository<Medicine, Long> {
 }
